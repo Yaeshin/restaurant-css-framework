ae287a79c1d41a655c707f93de6d28f5ca060228
Celestin Sivixay <celestin42@hotmail.fr>
2023-10-10 17:01:52 +0200

    remove useless import

--- shirashi/src/js/main.js
+++ shirashi/src/js/main.js
@@ -1,10 +1,6 @@
 // Import our custom CSS
 import '../styles/styles.css'
 
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-
-
 /* Javascript to show and hide cookie banner using localstorage */
 /* Shows the Cookie banner */
 function showCookieBanner(){
